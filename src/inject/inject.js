d8137600b9917cf88ed8cd078872dff1bb0e5d34
Piyush Santwani <psantwani@gmail.com>
2018-03-04 22:34:41 +0530

    findString logic updated

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,13 +2,18 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == "find_best_option") {
     let { q, qOptions } = request.payload;
     let result = {};
+    let body = document.getElementById("res").innerText.toLocaleLowerCase();
     qOptions.forEach(option => {
-      var found = findString(option);
+      option = option.toLocaleLowerCase();
+      var found = findString(body, option);
       if (found) {
-        result[option] = 1;
+        result[option] = found;
       }
     });
+
+    alert(JSON.stringify(result, null, 4));
     
+    /**
     let keys = Object.keys(result);
     if(keys.length > 1){
       keys.forEach(option => {
@@ -21,12 +26,21 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     else{
       alert(JSON.stringify(result, null, 4));
     }
+    **/
 
     sendResponse({ success: true });
   }
 });
 
-function findString(str, getCount=false) {
+function findString(str, option){
+  var regex = new RegExp(option, "g"), result, indices = [];
+  while ( (result = regex.exec(str)) ) {
+      indices.push(result.index);
+  }
+  return indices.length;
+}
+
+function findString2(str, getCount=false) {
   let count = 1;
   if (parseInt(navigator.appVersion) < 4) return;
   var strFound;
@@ -68,4 +82,4 @@ function findString(str, getCount=false) {
 }
 
 // https://www.google.co.in/search?game=quizmaster&q=hello&options=adele;tom hanks
-// https://www.google.co.in/search?q=who+has+been+india%27s+longest-+serving+prime+minister&game=quizmaster&options=options=jawaharlal nehru;indira gandhi;manmohan singh
+// https://www.google.co.in/search?num=5&q=who+has+been+india%27s+longest-+serving+prime+minister&game=quizmaster&options=options=jawaharlal nehru;indira gandhi;manmohan singh
